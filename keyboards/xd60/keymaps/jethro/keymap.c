f6488d3490fe29633eda772fde842f3eaf623739
Jethro Kuan <jethrokuan95@gmail.com>
2017-07-20 14:32:07 +0800

    change esc to grave and remove backspace

--- keyboards/xd60/keymaps/jethro/keymap.c
+++ keyboards/xd60/keymaps/jethro/keymap.c
@@ -23,11 +23,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // 0: Base Layer
   KEYMAP(
-         KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4, KC_5,    KC_6,    KC_7,      KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
-         KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R, KC_T,    KC_Y,    KC_U,      KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
-         CTLBSPC, KC_A,    KC_S,    KC_D,   KC_F, KC_G,    KC_H,    KC_J,      KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT, \
-         KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V, KC_B,    KC_N,    KC_M,      KC_COMM, KC_DOT, KC_SLSH, KC_UP,   KC_DEL, \
-         KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, F(0), KC_LEFT, KC_DOWN, KC_RIGHT), 
+         KC_GRAVE, KC_1,    KC_2,    KC_3,   KC_4, KC_5,    KC_6,    KC_7,      KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_ESC,  \
+         KC_TAB,   KC_Q,    KC_W,    KC_E,   KC_R, KC_T,    KC_Y,    KC_U,      KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
+         CTLBSPC,  KC_A,    KC_S,    KC_D,   KC_F, KC_G,    KC_H,    KC_J,      KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT, \
+         KC_LSFT,  KC_Z,    KC_X,    KC_C,   KC_V, KC_B,    KC_N,    KC_M,      KC_COMM, KC_DOT, KC_SLSH, KC_UP,   KC_DEL, \
+         KC_LCTL,  KC_LGUI, KC_LALT, KC_SPC, F(0), KC_LEFT, KC_DOWN, KC_RIGHT), 
 
   // 1: Function Layer
   KEYMAP(
@@ -39,26 +39,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-  // Custom Actions
-  const uint16_t PROGMEM fn_actions[] = {
+// Custom Actions
+const uint16_t PROGMEM fn_actions[] = {
   [0] = ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
-  };
+};
 
-  // Macros
-  const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+// Macros
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
 
   // MACRODOWN only works in this function
   switch(id) {
   case 0:
-  if (record->event.pressed) { register_code(KC_RSFT); }
-  else { unregister_code(KC_RSFT); }
-  break;
+    if (record->event.pressed) { register_code(KC_RSFT); }
+    else { unregister_code(KC_RSFT); }
+    break;
   }
 
   return MACRO_NONE;
-  };
+};
 
-  // Loop
-  void matrix_scan_user(void) {
+// Loop
+void matrix_scan_user(void) {
   // Empty
-  };
+};
