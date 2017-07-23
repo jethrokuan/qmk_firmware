2fc0b7c666456395ef5e83081ab448178ff905b4
Jethro Kuan <jethrokuan95@gmail.com>
2017-07-23 21:02:40 +0800

    Annotate keymaps

--- keyboards/xd60/keymaps/jethro/keymap.c
+++ keyboards/xd60/keymaps/jethro/keymap.c
@@ -21,7 +21,22 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  // 0: Base Layer
+  // 0: Base Layer (QWERTY)
+  /*
+
+   ╭────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────────╮
+   | GRV│  1 │ 2  │ 3  │  4 │ 5  │  6 │  7 │  8 │  9 │  0 │ -  │ =  |   ESC  │
+   ├────┴┬───┴┬─-─┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───────┤
+   │ TAB | Q  | W  │ E  |  R │ T  │ Y  │ U  │ I  │ O  │ P  │ [  │ ]  │   \   │
+   ├─────┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴───────┤
+   │C/BSP │ A  │ S  │ D  │ F  │ G  │ H  │ J  │ K  │ L  │ ;  │ '  | ENT       │
+   ├──────┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴──────┬────┤
+   │ LSHFT │ Z  │ X  │ C  │ V  │ B  │ N  │ M  │ ,  │ .  │ /  │ UP       | DEL│
+   ├──────┬┴────┴┬───┴──┬─┴────┴────┴────┴────┴────┴─┬──┴──┬─┴───┬────┬─┴────┤
+   │ LCTL │LGUI  │ LALT │           SPC              │ F(0)|LEFT │DOWN| RGHT │
+   ╰──────┴──────┴──────┴────────────────────────────┴─────┴─────┴────┴──────╯
+
+  */
   KEYMAP(
          KC_GRAVE, KC_1,    KC_2,    KC_3,   KC_4, KC_5,    KC_6,    KC_7,      KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_ESC,  \
          KC_TAB,   KC_Q,    KC_W,    KC_E,   KC_R, KC_T,    KC_Y,    KC_U,      KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
@@ -30,8 +45,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_LCTL,  KC_LGUI, KC_LALT, KC_SPC, F(0), KC_LEFT, KC_DOWN, KC_RIGHT), 
 
   // 1: Function Layer
+
+  /*
+
+    ╭────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────────╮
+    | ESC│ F1 │ F2 │ F3 │ F4 │ F5 │ F6 │ F7 │ F8 │ F9 │ F10│ F11│ F12|  RESET │
+    ├────┴┬───┴┬─-─┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───────┤
+    │     | MUP| UP │ MDN|BSPC│ HME│ CLC│    │ INS│    │ PSC│ LCK|PAUS|  DEL  │
+    ├─────┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴───────┤
+    │      │ LFT│ DWN│ RHT│ DEL│ END|PDWN│    │    │    │ HME│ PUP| ENT       │
+    ├──────┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴──────┬────┤
+    │ LSHFT │    │ APP│ BTP|    │    |VOLD│VOLU│MUTE│ END│PDWN| PUP      | INS│
+    ├──────┬┴────┴┬───┴──┬─┴────┴────┴────┴────┴────┴─┬──┴──┬─┴───┬────┬─┴────┤
+    │ LCTL │LGUI  │ LALT │           SPC              │ F(0)|LEFT │PDWN| END  │
+    ╰──────┴──────┴──────┴────────────────────────────┴─────┴─────┴────┴──────╯
+
+  */
+
   KEYMAP(
-         RESET,   KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,     KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_F13, \
+         KC_ESC,   KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,     KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  RESET, \
          KC_NO,   KC_WH_U, KC_UP,   KC_WH_D,  KC_BSPC, KC_HOME, KC_CALC,   KC_NO,   KC_INS,  KC_NO,  KC_PSCR, KC_SLCK, KC_PAUS, KC_DEL, \
          KC_NO,   KC_LEFT, KC_DOWN, KC_RIGHT, KC_DEL,  KC_END,  KC_PGDN,   KC_NO,   KC_NO,   KC_NO,  KC_HOME, KC_PGUP, KC_ENT, \
          KC_LSFT, KC_NO,   KC_APP,  BL_STEP,  KC_NO,   KC_NO,   KC_VOLD,   KC_VOLU, KC_MUTE, KC_END, KC_PGDN, KC_PGUP, KC_INS, \
