a100714e75d82625fd0f6d608da7a62dd4641fe9
Jethro Kuan <jethrokuan95@gmail.com>
2017-08-11 10:32:27 +0800

    update keymap

--- keyboards/xd60/keymaps/jethro/keymap.c
+++ keyboards/xd60/keymaps/jethro/keymap.c
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    ├─────┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴───────┤
    │C/BSP │ A  │ S  │ D  │ F  │ G  │ H  │ J  │ K  │ L  │ ;  │ '  | ENT       │
    ├──────┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴──────┬────┤
-   │ LSHFT │ Z  │ X  │ C  │ V  │ B  │ N  │ M  │ ,  │ .  │ /  │      UP  |F(0)│
+   │ LSHFT │ Z  │ X  │ C  │ V  │ B  │ N  │ M  │ ,  │ .  │ /  │      UP  |DEL │
    ├──────┬┴────┴┬───┴──┬─┴────┴────┴────┴────┴────┴─┬──┴──┬─┴───┬────┬─┴────┤
-   │ LCTL │LGUI  │ LALT │           SPC              │ F(1)|LEFT │DOWN| RGHT │
+   │ LCTL │LGUI  │ LALT │           SPC              │ F(0)|LEFT │DOWN| RGHT │
    ╰──────┴──────┴──────┴────────────────────────────┴─────┴─────┴────┴──────╯
 
   */
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_GRAVE, KC_1,    KC_2,    KC_3,   KC_4, KC_5,    KC_6,    KC_7,      KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
          KC_TAB,   KC_Q,    KC_W,    KC_E,   KC_R, KC_T,    KC_Y,    KC_U,      KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
          CTLBSPC,  KC_A,    KC_S,    KC_D,   KC_F, KC_G,    KC_H,    KC_J,      KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT, \
-         KC_LSFT,  KC_Z,    KC_X,    KC_C,   KC_V, KC_B,    KC_N,    KC_M,      KC_COMM, KC_DOT, KC_SLSH, KC_UP,   F(0), \
+         KC_LSFT,  KC_Z,    KC_X,    KC_C,   KC_V, KC_B,    KC_N,    KC_M,      KC_COMM, KC_DOT, KC_SLSH, KC_UP,   KC_DEL, \
          KC_LCTL,  KC_LGUI, KC_LALT, KC_SPC, F(0), KC_LEFT, KC_DOWN, KC_RIGHT), 
 
   // 1: Function Layer
@@ -59,9 +59,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ├─────┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴───────┤
     │      │VOLU│VOLD│MUTE│    │    |    │    │ 4  │ 5  │ 6  │ -  | /         │
     ├──────┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴┬───┴──────┬────┤
-    │       │PGUP│PGDN│HOME|END │    |    │    │  7 │ 8  │ 9  | 0        |F(0)│
+    │       │PGUP│PGDN│HOME|END │    |    │    │  7 │ 8  │ 9  | 0        |BSPC│
     ├──────┬┴────┴┬───┴──┬─┴────┴────┴────┴────┴────┴─┬──┴──┬─┴───┬────┬─┴────┤
-    │ RESET│      │      │                            │ BSPC| DOT │ENT |      │
+    │ RESET│      │      │                            │ F(0)| DOT │ENT |      │
     ╰──────┴──────┴──────┴────────────────────────────┴─────┴─────┴────┴──────╯
 
   */
@@ -70,8 +70,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_ESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,  KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
          KC_NO,  KC_PSCR, KC_SLCK, KC_PAUS, KC_NO,  KC_NO,   KC_NO,  KC_NO,  KC_1, KC_2, KC_3, N_PLUS, N_ASTK, KC_NO, \
          KC_NO,  KC_VOLU, KC_VOLD, KC_MUTE, KC_NO,  KC_NO,   KC_NO,  KC_NO,  KC_4, KC_5, KC_6, KC_MINUS, N_SLSH, \
-         KC_NO,  KC_PGUP, KC_PGDN, KC_HOME, KC_END, KC_NO,   KC_NO,  KC_NO,  KC_7, KC_8, KC_9, KC_0, F(0), \
-         RESET,  KC_NO,   KC_NO,   KC_NO,   KC_BSPC,  KC_DOT, KC_ENT, KC_NO),
+         KC_NO,  KC_PGUP, KC_PGDN, KC_HOME, KC_END, KC_NO,   KC_NO,  KC_NO,  KC_7, KC_8, KC_9, KC_0, KC_BSPC, \
+         RESET,  KC_NO,   KC_NO,   KC_NO,   F(0),  KC_DOT, KC_ENT, KC_NO),
 };
 
 // Custom Actions
